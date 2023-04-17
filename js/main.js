941bcdbdc3c4378d4203de83e1d97d89221dbe83
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-04-17 15:58:40 +0200

    da aggiungere i colori

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const containerr = document.getElementById("container");
+const ul = document.querySelector("ul.list");
+
+for (let i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) containerr.innerHTML += `<div class="box">"FizzBuzz"</div>`;
+    else if (i % 3 === 0)  containerr.innerHTML += `<div class="box">Fizz</div>`;
+    else if (i % 5 === 0) containerr.innerHTML += `<div class="box">Buzz</div>`;
+    else  containerr.innerHTML += `<div class="box">${i}</div>`;
+    const nuovoElementoInLista = document.createElement('li');
+    nuovoElementoInLista.append(i); 
+    ul.append(nuovoElementoInLista); 
+}
