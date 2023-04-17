4a9e757b42646548a39dd96a9c825db8a2d0fea5
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-04-17 16:03:29 +0200

    ok corretta la lista di numeri sotto

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ for (let i = 1; i < 101; i++) {
     else if (i % 3 === 0)  containerr.innerHTML += `<div class="box">Fizz</div>`;
     else if (i % 5 === 0) containerr.innerHTML += `<div class="box">Buzz</div>`;
     else  containerr.innerHTML += `<div class="box">${i}</div>`;
-    const nuovoElementoInLista = document.createElement('li');
-    nuovoElementoInLista.append(i); 
-    ul.append(nuovoElementoInLista); 
+   
 }
+const nuovoElementoInLista = document.createElement('li');
+nuovoElementoInLista.append(i); 
+ul.append(nuovoElementoInLista); 
