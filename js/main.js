ae94bd4008ba2ee73b5bf64df06616a6add86557
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-04-17 16:19:35 +0200

    soluzione pigra

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 const containerr = document.getElementById("container");
-const ul = document.querySelector("ul.list");
-
 for (let i = 1; i < 101; i++) {
-    if (i % 3 === 0 && i % 5 === 0) containerr.innerHTML += `<div class="box">"FizzBuzz"</div>`;
-    else if (i % 3 === 0)  containerr.innerHTML += `<div class="box">Fizz</div>`;
-    else if (i % 5 === 0) containerr.innerHTML += `<div class="box">Buzz</div>`;
+    if (i % 3 === 0 && i % 5 === 0) containerr.innerHTML += `<div class="box-1">"FizzBuzz"</div>`;
+    else if (i % 3 === 0)  containerr.innerHTML += `<div class="box-2">Fizz</div>`;
+    else if (i % 5 === 0) containerr.innerHTML += `<div class="box-3">Buzz</div>`;
     else  containerr.innerHTML += `<div class="box">${i}</div>`;
-   
 }
-const nuovoElementoInLista = document.createElement('li');
-nuovoElementoInLista.append(i); 
-ul.append(nuovoElementoInLista); 
+
